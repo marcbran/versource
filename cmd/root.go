a3dee54fc133e11e7f0a8ff18556c576263735af
Marc Brandenburg <git@marcbrandenburg.net>
2025-03-25 22:09:56 +0100

    feat: improved list and run

--- cmd/root.go
+++ cmd/root.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"os"
+	"path"
 )
 
 var rootCmd = &cobra.Command{
@@ -16,6 +18,7 @@ func init() {
 	rootCmd.AddCommand(initCmd)
 	rootCmd.AddCommand(syncCmd)
 	rootCmd.AddCommand(listCmd)
+	rootCmd.AddCommand(runCmd)
 }
 
 func Execute() {
@@ -23,4 +26,17 @@ func Execute() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+func logFile(dataDir string) (*os.File, error) {
+	file, err := os.OpenFile(path.Join(dataDir, "debug.log"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		return nil, err
+	}
+	log.SetOutput(file)
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp: true,
+	})
+	log.SetLevel(log.InfoLevel)
+	return file, nil
 }
